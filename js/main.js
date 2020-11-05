edca009764346c8941ce04ffc498a601b0c6cf3f
phoebe njoroge <phoebe.njoroge@gmail.com>
2020-11-05 16:53:34 -0600

    First Practice file

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log("Hi this is a test");
+console.log("This is also another test");
+alert("This is a scam");
+alert("Haha, I kid");
+
+var myAlert = "This is another aler";
+alert(myAlert);
+
+var confimed =confirm("Are you sure you want to continue?");
+console.log(confimed);
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
